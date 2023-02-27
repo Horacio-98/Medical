4b00b3fbe39e5c530840842f7e344c1ab295f607
Horacio Granados <granadoshoracio2@gmail.com>
2023-02-26 19:20:18 -0600

    Static Menu

--- Main.java
+++ Main.java
@@ -1,10 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         doctor myDoctor_1 = new doctor("Abel Granados", "General");  
-        myDoctor_1.showName();
         doctor myDoctor_2 = new doctor("Jeanett Argumedo", "General"); 
-        myDoctor_2.showName();
 
+        uiMenu.showMenu(); 
 
     }
 }
