27f732ba12502df31733eef23bf58b9983878a06
Horacio Granados <granadoshoracio2@gmail.com>
2023-03-02 02:40:54 -0600

    Getters and Setters

--- Main.java
+++ Main.java
@@ -1,9 +1,21 @@
+import javax.print.attribute.IntegerSyntax;
+
 public class Main {
     public static void main(String[] args) {
         //doctor myDoctor_1 = new doctor("Abel Granados", "General");  
         //doctor myDoctor_2 = new doctor("Jeanett Argumedo", "General"); 
         patient patient_1 = new patient("Horacio Granados", "granadoshoracio2@gmail.com"); 
-        
+        patient patient_2 = new patient("Beatriz Granados", "betyga@gmail.com");
+
+        patient_1.setHeight(1.70);
+        patient_1.setWeight(70);
+        patient_1.setPhone("56756864");
+        patient_1.setEmail("Prueba");
+        System.out.println("Altura : " + patient_1.getHeight());
+        System.out.println("Peso : " + patient_1.getWeight());
+        System.out.println("Telefono : " + patient_1.getPhone());
+        System.out.println("Email : " + patient_1.getEmail());
+
 
         //uiMenu.showMenu(); 
 
--- patient.java
+++ patient.java
@@ -1,4 +1,7 @@
 public class patient {
+
+    // Atributos
+
     private String name; 
     private String email; 
     private String address; 
@@ -8,9 +11,97 @@ public class patient {
     private double height; 
     private String blood; 
 
+
+    // Constructor
+
     public patient(String name, String email){
         this.name = name; 
         this.email = email; 
     }
+
+
+    // Getters and Setters
+
+    public String getName(){
+        return name; 
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public String getWeight() {
+        return this.weight + " kg";
+    }
+
+    public String getHeight() {
+        return this.height + " m";
+    }
+
+    public String getBlood() {
+        return blood;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        if (email.contains("@")) {
+            this.email = email;
+        }
+        else {
+            System.out.println("El formato no es valido para un correo");
+            System.out.println("El correo no se ha cambiado");
+        }
+        
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setPhone(String phone) {
+        if (phone.length() == 8){
+            this.phone = phone;
+        }
+        else{
+            System.out.println("El telefono debe contener 8 digitos");
+        }
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public void setBlood(String blood) {
+        this.blood = blood;
+    }
+
+    
+
+    
+
+
     
 }
