f24275048065923bbaee8950bb62dc1c0bc6dfd5
Horacio Granados <granadoshoracio2@gmail.com>
2023-03-10 03:11:13 -0600

    Back to the code

--- Main.java
+++ Main.java
@@ -1,11 +1,9 @@
-import javax.print.attribute.IntegerSyntax;
-
 public class Main {
     public static void main(String[] args) {
         //doctor myDoctor_1 = new doctor("Abel Granados", "General");  
         //doctor myDoctor_2 = new doctor("Jeanett Argumedo", "General"); 
         patient patient_1 = new patient("Horacio Granados", "granadoshoracio2@gmail.com"); 
-        patient patient_2 = new patient("Beatriz Granados", "betyga@gmail.com");
+        //patient patient_2 = new patient("Beatriz Granados", "betyga@gmail.com");
 
         patient_1.setHeight(1.70);
         patient_1.setWeight(70);
@@ -16,6 +14,8 @@ public class Main {
         System.out.println("Telefono : " + patient_1.getPhone());
         System.out.println("Email : " + patient_1.getEmail());
 
+
+        System.out.println("Vamos en el id : " + doctor.id);
 
         //uiMenu.showMenu(); 
 
